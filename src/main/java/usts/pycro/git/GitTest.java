374e47d1a998007aa84dd3aa6bbbc8c7776bb982
Pycro <2023747015@qq.com>
2023-03-18 14:49:23 +0800

    push commit

--- src/main/java/usts/pycro/git/GitTest.java
+++ src/main/java/usts/pycro/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello,git4!");
         System.out.println("master commit");
         System.out.println("hot-fix commit2");
+        System.out.println("push commit");
     }
 }

c9be670dd5a882cbebab0cc5a33145ada4b2ee55
Pycro <102418077+zeta2333@users.noreply.github.com>
2023-03-18 14:58:25 +0800

    pull commit

--- src/main/java/usts/pycro/git/GitTest.java
+++ src/main/java/usts/pycro/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("master commit");
         System.out.println("hot-fix commit2");
         System.out.println("push commit");
+        System.out.println("pull commit");
     }
 }

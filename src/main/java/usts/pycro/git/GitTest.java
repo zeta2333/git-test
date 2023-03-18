938fca206fd744c020fe93984806049080506ae2
Pycro <2023747015@qq.com>
2023-03-18 13:32:46 +0800

    first commit

---
+++ src/main/java/usts/pycro/git/GitTest.java
@@ -0,0 +1,13 @@
+package usts.pycro.git;
+
+/**
+ * @Author Pycro
+ * @Create 2023-03-18  13:31
+ * @Description
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
